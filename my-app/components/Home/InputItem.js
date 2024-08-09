2ad1691a2d31e07e7258ec2ea7a16e6cf818920c
Aritra Chatterjee <161926590+aritrachatterjee7@users.noreply.github.com>
2024-08-09 16:02:16 +0000

    google maps api

--- my-app/components/Home/InputItem.js
+++ my-app/components/Home/InputItem.js
@@ -1,29 +1,39 @@
-/*import React from 'react'
-
-const InputItem = () => {
-  return (
-    <div className='bg-slate-200 p-3 rounded-lg mt-3'>
-      <input type='text' placeholder={type=='source'?'Pickup Location':'Drop Location'}
-      className='bg-transparent w-full outline-none'/>
-    </div>
-  )
-}
-
-export default InputItem*/
-
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 const InputItem = ({ type }) => {
+  const [value, setValue] = useState(null);
+
   return (
-    <div className='bg-slate-200 p-3 rounded-lg mt-3'>
-      <input 
-        type='text' 
-        placeholder={type === 'source' ? 'Pickup Location' : 'Drop Location'}
-        className='bg-transparent w-full outline-none'
+    <div className="bg-slate-200 p-3 rounded-lg mt-3">
+      <GooglePlacesAutocomplete
+        apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+        selectProps={{
+          value,
+          onChange: setValue,
+          placeholder: type === "source" ? "Pickup Location" : "Drop Location",
+          isClearable: true,
+          className: "w-full",
+          components: {
+            DropdownIndicator: () => null, // Hide the dropdown indicator
+          },
+          styles: {
+            control: (provided) => ({
+              ...provided,
+              backgroundColor: "transparent",
+              border: "none",
+              boxShadow: "none", // Remove the box shadow
+            }),
+            menu: (provided) => ({
+              ...provided,
+              zIndex: 9999, // Ensure the dropdown appears above other elements
+            }),
+          },
+        }}
       />
     </div>
-  )
-}
+  );
+};
 
-export default InputItem
-
+export default InputItem;
