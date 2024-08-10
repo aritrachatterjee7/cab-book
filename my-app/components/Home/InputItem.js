2e02300da27123a8271754c09be02c230c12c185
Aritra Chatterjee <161926590+aritrachatterjee7@users.noreply.github.com>
2024-08-10 04:54:23 +0000

    get source/destination

--- my-app/components/Home/InputItem.js
+++ my-app/components/Home/InputItem.js
@@ -1,9 +1,41 @@
 "use client";
-import React, { useState } from "react";
+import { DestinationContext } from "@/context/DestinationContext";
+import { SourceContext } from "@/context/SourceContext";
+import React, { useContext, useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 const InputItem = ({ type }) => {
   const [value, setValue] = useState(null);
+  const { source, setSource } = useContext(SourceContext);
+  const { destination, setDestination } = useContext(DestinationContext);
+
+  const getLatAndLng = (place, type) => {
+    const placeId = place.value.place_id;
+    const service = new google.maps.places.PlacesService(document.createElement('div'));
+    service.getDetails({ placeId }, (result, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK && result.geometry && result.geometry.location) {
+        const lat = result.geometry.location.lat();
+        const lng = result.geometry.location.lng();
+
+        const locationData = {
+          lat: lat,
+          lng: lng,
+          name: result.formatted_address,
+          label: result.name
+        };
+
+        if (type === 'source') {
+          setSource(locationData);
+          console.log("Source:", locationData);  // Log source only when it's set
+        } else {
+          setDestination(locationData);
+          console.log("Destination:", locationData);  // Log destination only when it's set
+        }
+      } else {
+        console.error('Failed to get place details');
+      }
+    });
+  };
 
   return (
     <div className="bg-slate-200 p-3 rounded-lg mt-3">
@@ -11,7 +43,10 @@ const InputItem = ({ type }) => {
         apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
         selectProps={{
           value,
-          onChange: setValue,
+          onChange: (place) => {
+            getLatAndLng(place, type);
+            setValue(place);
+          },
           placeholder: type === "source" ? "Pickup Location" : "Drop Location",
           isClearable: true,
           className: "w-full",
