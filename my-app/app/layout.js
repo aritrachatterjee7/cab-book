54e8ab1512ae1fe2a3e2dfd09389d78aefd7737f
Aritra Chatterjee <161926590+aritrachatterjee7@users.noreply.github.com>
2024-08-16 06:59:35 +0000

    errors fixed

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -4,12 +4,12 @@ import Header from '@/components/Header';
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider fallbackRedirectUrl="/dashboard">
+    <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className='bg-gray-100'>
           <Header />
           <SignedOut>
-            <div className="flex items-center justify-center min-h-screen overflow-hidden bg-gray-100">
+            <div className="flex items-center justify-center min-h-screen bg-gray-100">
               <SignIn routing="hash" />
             </div>
           </SignedOut>
--- my-app/app/sign-up/[[...sign-up]]/page.js
+++ my-app/app/sign-up/[[...sign-up]]/page.js
@@ -1,8 +1,8 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignUp } from '@clerk/nextjs';
 
-export default function Page() {
+export default function SignUpPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md">
         <SignUp routing="hash" />
       </div>
