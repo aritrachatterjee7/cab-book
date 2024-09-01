e066fdffd7852e211d3803012bfab7b030b0d452
Aritra Chatterjee <161926590+aritrachatterjee7@users.noreply.github.com>
2024-09-01 16:01:32 +0000

    errors fixed

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className='bg-gray-100'>
+        <body className="bg-gray-100">
           <Header />
           <SignedOut>
             <div className="flex items-center justify-center min-h-screen bg-gray-100">
--- my-app/app/sign-up/[[...sign-up]]/page.js
+++ my-app/app/sign-up/[[...sign-up]]/page.js
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs';
 export default function SignUpPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md">
+      <div className="w-full max-w-md p-4">
         <SignUp routing="hash" />
       </div>
     </div>
